e1546395cf1cbb7018abc2195187ad34629f731c
Nak <inyakimd@gmail.com>
2018-02-16 17:33:44 +0100

    [FIX] Transparency, frame & alwaysonTop window

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const notifyBtn = document.getElementById('notifyBtn');
 
 notifyBtn.addEventListener('click', function (event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
-    let win = new BrowserWindow({ frame:false, transparent: true, width: 400, height: 200 })
+    let win = new BrowserWindow({ frame:false, transparent: true, alwaysOnTop: true, width: 400, height: 200 })
     win.on('close', function () { win = null; })
     win.loadURL(modalPath)
     win.show()
