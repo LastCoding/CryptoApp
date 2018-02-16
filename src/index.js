de47bcccf2f2d3411fc5d517a92ad987787eed49
Nak <inyakimd@gmail.com>
2018-02-16 18:40:34 +0100

    Added cryptocoin price from BTC to EUR & 30s price auto-refresh

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const axios = require('axios')
 
 const notifyBtn = document.getElementById('notifyBtn');
+var price = document.querySelector('h1')
+var targetPrice = document.getElementById('targetPrice')
+
+function getBTC() {
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=EUR')
+        .then(res => {
+            const cryptos = res.data.BTC.EUR
+            price.innerHTML = '€'+cryptos.toLocaleString('es')
+        })
+}
+getBTC()
+setInterval(getBTC, 30000);
 
 notifyBtn.addEventListener('click', function (event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
